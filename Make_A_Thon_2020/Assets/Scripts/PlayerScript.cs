8f5f1507818abe26d835ce27785cfb55cbd7dd45
max <max.vandewege@xs4all.nl>
2020-06-04 12:00:13 +0200

    final changes ;'-(

--- Make_A_Thon_2020/Assets/Scripts/PlayerScript.cs
+++ Make_A_Thon_2020/Assets/Scripts/PlayerScript.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    void Start()
-    {
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
 
-    }
+    public int score = 0;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     void Update()
     {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        if (Input.GetButtonDown("Jump1") || Input.GetButtonDown("Jump2"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            score += 100;
+        }
     }
 }
